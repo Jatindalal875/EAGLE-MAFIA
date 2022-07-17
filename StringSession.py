c955fbfc3fd4981e659d32e25f222cf58a097891
UNKNOWN <79321850+Jatindalal875@users.noreply.github.com>
2022-07-18 00:50:31 +0530

    Create StringSession.py

---
+++ StringSession.py
@@ -0,0 +1,29 @@
+print(
+    "ㅤ\nㅤ\n\n\n\nㅤ\n┈┈┏━╮╭━┓┈╭━━━━━━╮\n┈┈┃┏┗┛┓┃╭┫EAGLEMAFIA SPAMUSERBOT ┃\n┈┈╰┓▋▋┏╯╯╰━━━━━━╯\n┈╭━┻╮╲┗━━━━╮╭╮┈\n┈┃▎▎┃╲╲╲╲╲╲┣━╯┈\n┈╰━┳┻▅╯╲╲╲╲┃┈┈┈\n┈┈┈╰━┳┓┏┳┓┏╯┈┈┈\n┈┈┈┈┈┗┻┛┗┻┛┈┈┈┈┏━╮╭━┓┈╭━━━━━━╮\n┈┈┃┏┗┛┓┃╭┫EAGLEMAFIA SPAMUSERBOT ┃\n┈┈╰┓▋▋┏╯╯╰━━━━━━╯\n┈╭━┻╮╲┗━━━━╮╭╮┈\n┈┃▎▎┃╲╲╲╲╲╲┣━╯┈\n┈╰━┳┻▅╯╲╲╲╲┃┈┈┈\n┈┈┈╰━┳┓┏┳┓┏╯┈┈┈\n┈┈┈┈┈┗┻┛┗┻┛┈┈┈┈┏━╮╭━┓┈╭━━━━━━╮\n┈┈┃┏┗┛┓┃╭┫EAGLEMAFIA SPAMUSERBOT ┃\n┈┈╰┓▋▋┏╯╯╰━━━━━━╯\n┈╭━┻╮╲┗━━━━╮╭╮┈\n┈┃▎▎┃╲╲╲╲╲╲┣━╯┈\n┈╰━┳┻▅╯╲╲╲╲┃┈┈┈\n┈┈┈╰━┳┓┏┳┓┏╯┈┈┈\n┈┈┈┈┈┗┻┛┗┻┛┈┈┈┈┏━╮╭━┓┈╭━━━━━━╮\n┈┈┃┏┗┛┓┃╭┫EAGLEMAFIA SPAMUSERBOT ┃\n┈┈╰┓▋▋┏╯╯╰━━━━━━╯\n┈╭━┻╮╲┗━━━━╮╭╮┈\n┈┃▎▎┃╲╲╲╲╲╲┣━╯┈\n┈╰━┳┻▅╯╲╲╲╲┃┈┈┈\n┈┈┈╰━┳┓┏┳┓┏╯┈┈┈\n┈┈┈┈┈┗┻┛┗┻┛┈┈"
+)
+print("\n\n\n\n\nABE GANDU SUNN TU STRING SESSION GENERATE KARNA KYA TU IDHAR UDHAR LADKI KO DEKH RAHA VO SETTING HAI HAMARI. ABE CHAL NA CREATE KAR STRING SESSION")
+print("\n\nEAGLEMAFIA SPAMUSERBOT ")
+
+print("\n\nProperly Fill APP_ID ,HASH and Number.\n")
+
+from telethon.sync import TelegramClient
+from telethon.sessions import StringSession
+APP_ID = int(input("Enter APP ID here: "))
+API_HASH = input("Enter API HASH here: ")
+with TelegramClient(StringSession(), APP_ID, API_HASH) as hehe:
+	dcsession = hehe.session.save()
+	dEAGLE = hehe.send_message(
+	    "me",
+	    f"`{dcsession}`\n\n**Your EAGLEMAFIA-USPAMBOT String Session Here Sir😁😎😎\nClick on above Code to Copy it\n\nFor Support Join** @BROTHERHOOD_SUPPORT"
+	)
+
+print("\n\n############################\n")
+print(
+    "IDHAR KUCH NHI H BE GANDU SUNN TU APNE ACCOUNT KA SAVE MESSAGES OPEN KAR OR UDHAR JAA KAR DEKHLE STRING SESSION PADA HOGA 😂😂. ")
+
+print("\n############################\n")
+
+
+print(f"{dcsession}")
+
+Print(" ")
